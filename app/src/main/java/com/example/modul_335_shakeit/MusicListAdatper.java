c508e62826867d79bc47286473aa79ac027310b1
INTRANET\Khalilou.Kourouma <k.kourouma@hotmail.com>
2023-03-07 11:44:48 +0100

    add viewHolder methods to MusicListAdapter

--- app/src/main/java/com/example/modul_335_shakeit/MusicListAdatper.java
+++ app/src/main/java/com/example/modul_335_shakeit/MusicListAdatper.java
@@ -1,13 +1,51 @@
 package com.example.modul_335_shakeit;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class MusicListAdatper {
+import java.util.ArrayList;
+
+public class MusicListAdatper extends RecyclerView.Adapter<MusicListAdatper.ViewHolder> {
+
+    ArrayList<AudioFileModel> songs;
+    Context context;
+
+    public MusicListAdatper(ArrayList<AudioFileModel> songsList, Context context) {
+        this.songs = songsList;
+        this.context = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder( ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recycler_item,parent,false);
+        return new MusicListAdatper.ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        AudioFileModel songData = songs.get(position);
+        holder.titleTextView.setText(songData.getTitle());
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView titleTextView;
+        ImageView iconImageView;
         public ViewHolder(View itemView) {
             super(itemView);
+            titleTextView = itemView.findViewById(R.id.music_title_text);
+            iconImageView = itemView.findViewById(R.id.icon_view);
         }
     }
 }
